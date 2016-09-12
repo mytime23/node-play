76b64ccab4613bb461e424c07a93a776fa2a72a3
Tim Wan <Tim Wan>
2016-09-12 15:28:15 +0800

    build a simple REST API for led

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ var path = require('path');
 
 var app = express();
 
-app.set('views', './views'); 
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
      res.sendFile(path.join(__dirname + '/view/index.html'));
 });
+
+app.get('/led/:id', function(req, res) {
+    const id = req.params.id;
+    if(id == 0) 
+        res.sendFile(path.join(__dirname + '/public/images/S3_Icon_00(m).png'));
+    else
+        res.sendFile(path.join(__dirname + '/public/images/S3_Icon_01(m).png'));
+})
 
 app.listen(3000, function() {
     console.log('http://localhost:3000 is listened ! ');
